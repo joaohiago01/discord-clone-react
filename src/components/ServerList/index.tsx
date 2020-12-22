914ee51efac915f03164ddcccfe1552c9967dbda
joaohiago01 <joaohiago96@gmail.com>
2020-12-22 15:46:17 -0300

    Init Discord Clone with React.js

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton';
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+  <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton hasNotifications />
+      <ServerButton mentions={5} />
+  </Container>
+  );
+}
+
+export default ServerList;
