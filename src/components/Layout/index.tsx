81410d7acda2be74ce3cfbb20a743201a10ce3a8
joaohiago01 <joaohiago96@gmail.com>
2021-01-07 07:57:23 -0300

    Add component ChannelInfo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import ServerList from '../ServerList';
 import ServerName from '../ServerName';
+import ChannelInfo from '../ChannelInfo';
 
 import { Grid } from './styles';
 
@@ -10,6 +11,7 @@ const Layout: React.FC = () => {
       <Grid>
           <ServerList />
           <ServerName />
+          <ChannelInfo />
       </Grid>
   );
 }
