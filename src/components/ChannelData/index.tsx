5ee92efd4d091c40171dafb743a5c867b535fc55
joaohiago01 <joaohiago96@gmail.com>
2021-01-13 08:56:06 -0300

    Create ChannelMessage component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return (
       <Container>
-        <Messages />
+        <Messages ref={messagesRef}>
+          <ChannelMessage 
+            author='Gwynbleidd'
+            date='15/01/2021'
+            content='Sextou com Among Us'
+          />
+
+          <ChannelMessage 
+            author='Torgate'
+            date='15/01/2021'
+            content={
+              <>
+                <Mention>@Gwynbleidd</Mention>, vou te passar a faca no Among Us.
+              </>
+            }
+          />
+        </Messages>
 
         <InputWrapper>
           <Input type="text" placeholder="Conversar em #chat-livre"/>
